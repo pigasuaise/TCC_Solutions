69a7c2b4c342587a7214c383d0f2509f62c5b74b
unknown <ozzytit@protonmail.com>
2025-05-15 18:56:40 -0300

    Adicionados comentários para explicar o código

--- TCC_NoData/apptccnodata/src/main/java/com/example/apptccnodata/Login/LoginController.java
+++ TCC_NoData/apptccnodata/src/main/java/com/example/apptccnodata/Login/LoginController.java
@@ -13,35 +13,42 @@ import com.example.apptccnodata.Login.Services.LoginService;
 
 import jakarta.servlet.http.HttpSession;
 
-
-
-
 @Controller
 public class LoginController {
     @Autowired
     private final LoginService loginService;
-    
+    /*
+     * Aqui acontece a mesma coisa do serviço do login, por mais que inútil, na visão do JAVA, o @Autowired ainda é uma 
+     * anotação necessária dentro deste código.
+     */
     @Autowired
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
 
-    @GetMapping("/index")
+    @GetMapping("/index") // Esta linha busca a requisição HTTP chamada get, que basicamente acontece quando você adiciona "/n" à um link, por exemplo
     public String showLogin() {
         return "index";
     }
     
     @PostMapping("/logar")
-    // RequestParam vai pegar os dados do form e trazer para o backend
-    public String processarLogin(@RequestParam String email, @RequestParam String senha, HttpSession session, RedirectAttributes redirectAttributes, Model model) {
+    public String processarLogin(@RequestParam String email, @RequestParam String senha, HttpSession session, RedirectAttributes redirectAttributes, Model model) {     // RequestParam vai pegar os dados do form e trazer para o backend
+        /*
+         * Como utilizamos diretamente a classe de loginService (cuja qual está diretamente ligada com o repositório), usar de um método
+         * para a validação sempre vai ser a opção mais segura.
+         * 
+         * Então aqui nós acessaremos a classe e buscaremos o método .authenticate para utilizar dos parametros do formulario HTML
+         * para efetuar o login da forma correta, evitando linhas exageradas.
+         */
         Usuario usuario = loginService.authenticate(email, senha);
 
-        if(usuario != null) {
+        if(usuario != null) { // Validamos se o retorno de usuário não é nulo. Se deve basicamente pelo reinicio da página ou debug de código, isso apaga a sessão!
             session.setAttribute("usuarioLogado", usuario);
             System.out.println("Usuario " + usuario.getNome() + " logou");
             return "redirect:/dashboard";
         } 
 
+        // Caso o usuário seja nulo, poderiamos, de fato, utilizar um else, mas optamos pela unica ultima opção lógica.
         redirectAttributes.addFlashAttribute("error", "Credenciais inválidas");
         return "redirect:/index";
     }
--- TCC_NoData/apptccnodata/src/main/java/com/example/apptccnodata/Login/Services/LoginService.java
+++ TCC_NoData/apptccnodata/src/main/java/com/example/apptccnodata/Login/Services/LoginService.java
@@ -14,18 +14,32 @@ public class LoginService {
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
 
+    // Por mais que o Spring indique que @Autowired é inútil, precisar ter esta anotação
+    // para utilizar tanto repositório do usuário quanto do chat.
     @Autowired
     public LoginService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /* 
+    Método para a validação do usuário, busca pela senha inicial (a senha que está no formulário de login) e 
+    compara com o Hash do banco de dados.
+
+    Está é uma classe bastante sensível, já que lida diretamente com os dados armazenados.
+    Cuidar dela é necessário, então quanto mais segurança melhor
+
+    Uma boa prática seria ofuscar este código, mas por enquanto ainda estamos trabalhando localmente
+    assim evitando erros quanto ao SQL Injection ou Rainbow Tables.
+    */
     public Usuario authenticate(String email, String rawSenha) {
+        // Aqui utiliza-se do método dentro de usuarioRepository
         Usuario usuario = usuarioRepository.findByEmail(email);
 
+        // Então, se usuario não for nulo e a senha bata com a senha do banco de dados, o usuário será logado.
         if(usuario != null && passwordEncoder.matches(rawSenha, usuario.getSenha())) {
             return usuario;
         }
-        return null;
+        return null; // e como temos apenas uma opção lógica, apenas digitamos abaixo que o ultimo retorno vai ser null.
     }
 }
